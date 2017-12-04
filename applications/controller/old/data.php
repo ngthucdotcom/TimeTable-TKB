32abfec5553eb77b8f079053d5673fa3a8f5f258
Nguyen Thuc <me@ngthuc.com>
2017-12-04 23:24:21 +0700

    Update models 201712042324

---
+++ applications/controller/old/data.php
@@ -0,0 +1,90 @@
+<?php
+//Khu vuc du lieu
+// $data = array(
+//       "id" => 18008198,
+//       "user" => "admin",
+//       "pwd" => "1234",
+//       "email" => "admin@tkb.tk",
+//       "name" => "Nguyên Thức",
+//       "analytics" => "UA-84266655-1",
+//       "desc" => "Mô tả của người dùng"
+// );
+//
+// $monhoc = array(
+//      array(
+//       "TEN_MON" => "ComIS",
+//       "PHONG" => "101/C1",
+//       "THU" => 4,
+//       "TIET_BD" => 3,
+//       "SO_TIET" => 3,
+//       "NHOM" => 02,
+//       "DESC" => "CT181 - Enterprise IS"
+//     ),
+//      array(
+//       "TEN_MON" => "PrjMng",
+//       "PHONG" => "203/C1",
+//       "THU" => 3,
+//       "TIET_BD" => 3,
+//       "SO_TIET" => 3,
+//       "NHOM" => 01,
+//       "DESC" => "CT223 - Software Project Management"
+//     ),
+// 	   array(
+//       "TEN_MON" => "Struc",
+//       "PHONG" => "305/D1",
+//       "THU" => 5,
+//       "TIET_BD" => 6,
+//       "SO_TIET" => 3,
+//       "NHOM" => 01,
+//       "DESC" => "CT242 -Software Structure"
+//     ),
+//      array(
+//       "TEN_MON" => "Test1",
+//       "PHONG" => "111/C2",
+//       "THU" => 3,
+//       "TIET_BD" => 1,
+//       "SO_TIET" => 2,
+//       "NHOM" => 01,
+//       "DESC" => "CT243 - Software Testing"
+//     ),
+//      array(
+//       "TEN_MON" => "Test2",
+//       "PHONG" => "101/C1",
+//       "THU" => 5,
+//       "TIET_BD" => 4,
+//       "SO_TIET" => 2,
+//       "NHOM" => 01,
+//       "DESC" => "CT243 - Software Testing"
+//     ),
+// 	   array(
+//       "TEN_MON" => ".NET",
+//       "PHONG" => "206/KH",
+//       "THU" => 6,
+//       "TIET_BD" => 1,
+//       "SO_TIET" => 3,
+//       "NHOM" => 03,
+//       "DESC" => "CT246 - .NET"
+//     ),
+// 	   array(
+//       "TEN_MON" => "SHCVHT",
+//       "PHONG" => "203/TS",
+//       "THU" => 4,
+//       "TIET_BD" => 9,
+//       "SO_TIET" => 1,
+//       "NHOM" => 027,
+//       "DESC" => "Week 1 - 4 - 8 - 13"
+//     ),
+// 	   array(
+//       "TEN_MON" => "NLCS",
+//       "PHONG" => "N/A",
+//       "THU" => 2,
+//       "TIET_BD" => 10,
+//       "SO_TIET" => 1,
+//       "NHOM" => 02,
+//       "DESC" => "CT239 - Not Schedule"
+//     )
+// );
+// var_dump($monhoc);
+// echo json_encode($monhoc, JSON_PRETTY_PRINT);
+// var_dump(json_decode(file_get_contents('http://localhost/tkb/applications/models/db.json'), true);
+?>
